c23405c7e27818c4d03c2783a300c80e962d5a38
jaymisal18 <jayshivshankarmisal18@gmail.com>
2026-09-10 22:57:49 +0530

    Time: 3 ms (86.05%), Space: 43.2 MB (72.54%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack <Character> st=new Stack<>();
+
+        for(int i=0 ;i<s.length();i++){
+            char curr=s.charAt(i);
+            if( curr=='(' || curr=='['|| curr=='{' ) {
+                st.push(curr);
+            }
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }
+
+               char top= st.peek();
+              
+
+               if( ( curr==')' && top=='(' ) || (curr==']' && top=='[') || (curr=='}' &&  top=='{') ){
+                 st.pop();
+               }
+
+               else{
+                return false;
+               }
+                  
+            }
+        }
+        return st.isEmpty();
+    }
+}
